b3e5549ed172f2ba7505667f795811efde51f510
Your Name <you@example.com>
2021-09-17 23:53:32 +0100

    new update

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -5,7 +5,7 @@ const SearchBox = ({onSearch}) => {
 
     return (
         <div >
-            <input onChange={onSearch} className="pa3 ba b--green bg-lightest-blue" type="search" name="" id="" />
+            <input onChange={onSearch} className="pa3 ba b--green bg-lightest-blue" type="search" name="searchbox" />
         </div>
     )
 }
