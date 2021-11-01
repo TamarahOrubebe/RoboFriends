5af2c4880df2adcfcd20c7dcd79dca7c2964de7c
Your Name <you@example.com>
2021-11-01 12:16:25 +0100

    add prettier

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import Card from './Card';
+import React from "react";
+import Card from "./Card";
 
-import { IRobot } from './MainPage';
+import { IRobot } from "./MainPage";
 
 interface IProp {
-    robots: Array<IRobot>
+  robots: Array<IRobot>;
 }
 
 const CardList = ({ robots }: IProp) => {
-    
-
-    return (
-
-        <div>
-             { robots.map((robot, i) => <Card key={i} name={robot.name} email={robot.email} id={robot.id} />) }
-        </div>
-      
-        
-        
-    );
-}
-
-
+  return (
+    <div>
+      {robots.map((robot, i) => (
+        <Card key={i} name={robot.name} email={robot.email} id={robot.id} />
+      ))}
+    </div>
+  );
+};
 
 export default CardList;
