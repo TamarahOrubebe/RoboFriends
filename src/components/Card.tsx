9165a86e03c65f9ace9220b92cb21e63c2e994c9
Your Name <you@example.com>
2021-11-01 12:12:54 +0100

    add prettier

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import { IRobot } from './MainPage';
+import React from "react";
+import { IRobot } from "./MainPage";
 
-
-const Card = ({ name, id, email } : IRobot) => {
-
-     
-
-    return (
-        <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5 tc">
-            <img src={`https://robohash.org/test${id}?200*200`} width="200" height="200" alt="robots" />
-            <div>
-                <h2>{name}</h2>
-                <p>{email}</p>
-            </div>
-        </div>
-    )
-}
-
+const Card = ({ name, id, email }: IRobot) => {
+  return (
+    <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5 tc">
+      <img
+        src={`https://robohash.org/test${id}?200*200`}
+        width="200"
+        height="200"
+        alt="robots"
+      />
+      <div>
+        <h2>{name}</h2>
+        <p>{email}</p>
+      </div>
+    </div>
+  );
+};
 
 export default Card;
